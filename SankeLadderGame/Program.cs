e18c2fed6cae42d066018f6b17b54ec9755fe4e9
KajalShendage <shendagekajal123@gmail.com>
2022-08-26 00:37:54 +0530

    [Pramod] Add: Postion of Player

--- SankeLadderGame/Program.cs
+++ SankeLadderGame/Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using SankeLadderGame;
+
+Game game = new Game();
+Console.WriteLine(game.playerPosition);
